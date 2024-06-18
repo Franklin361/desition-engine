80b3f4ff2e283a83da95fa9051dda7336048a789
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2024-06-18 16:29:28 -0600

    first commit

---
+++ motor.js
@@ -0,0 +1,242 @@
+
+const AMOUNT_MINIMUM_MALE = {
+  '26': {
+    'A': 100,
+    'B': 1000,
+    'C': 400,
+    'D': 400,
+  },
+  '27': {
+    'A': 400,
+    'B': 600,
+    'C': 200,
+    'D': 300,
+  },
+  '28': {
+    'A': 900,
+    'B': 1000,
+    'C': 200,
+    'D': 500,
+  },
+  '29': {
+    'A': 100,
+    'B': 1000,
+    'C': 1000,
+    'D': 900,
+  },
+  '30': {
+    'A': 600,
+    'B': 1000,
+    'C': 600,
+    'D': 1000,
+  },
+}
+const AMOUNT_MINIMUM_FEMALE = {
+  '24': {
+    'A': 800,
+    'B': 800,
+    'C': 200,
+    'D': 500,
+  },
+  '25': {
+    'A': 800,
+    'B': 700,
+    'C': 900,
+    'D': 1000,
+  },
+  '26': {
+    'A': 800,
+    'B': 100,
+    'C': 700,
+    'D': 600,
+  },
+  '27': {
+    'A': 600,
+    'B': 600,
+    'C': 800,
+    'D': 400,
+  },
+  '28': {
+    'A': 200,
+    'B': 700,
+    'C': 100,
+    'D': 700,
+  },
+}
+const AMOUNT_MAXIMUM_MALE = {
+  '26': {
+    'A': 4900,
+    'B': 4700,
+    'C': 5000,
+    'D': 4400,
+  },
+  '27': {
+    'A': 4700,
+    'B': 4400,
+    'C': 4700,
+    'D': 4700,
+  },
+  '28': {
+    'A': 4600,
+    'B': 5000,
+    'C': 5000,
+    'D': 4300,
+  },
+  '29': {
+    'A': 4600,
+    'B': 4400,
+    'C': 4200,
+    'D': 4900,
+  },
+  '30': {
+    'A': 4500,
+    'B': 4900,
+    'C': 4600,
+    'D': 4300,
+  },
+}
+const AMOUNT_MAXIMUM_FEMALE = {
+  '24': {
+    'A': 4000,
+    'B': 4700,
+    'C': 4600,
+    'D': 5000,
+  },
+  '25': {
+    'A': 4200,
+    'B': 4200,
+    'C': 4900,
+    'D': 4900,
+  },
+  '26': {
+    'A': 4100,
+    'B': 4500,
+    'C': 4600,
+    'D': 4700,
+  },
+  '27': {
+    'A': 4200,
+    'B': 4300,
+    'C': 4700,
+    'D': 5000,
+  },
+  '28': {
+    'A': 4500,
+    'B': 4400,
+    'C': 4000,
+    'D': 4300,
+  },
+}
+
+const GENRE = {
+  'f': {
+    amountMaximum: { ...AMOUNT_MAXIMUM_FEMALE },
+    amountMinimum: { ...AMOUNT_MINIMUM_FEMALE },
+  },
+  'm': {
+    amountMaximum: { ...AMOUNT_MAXIMUM_MALE },
+    amountMinimum: { ...AMOUNT_MINIMUM_MALE },
+  }
+}
+
+const getMonthsSinceFirstJob = (firstJobDate) => {
+
+  const regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+
+  if (!firstJobDate || !regex.test(firstJobDate)) throw new Error('Invalid first job date')
+
+  const [day, month, year] = firstJobDate.split('/');
+
+  const todayTime = new Date().getTime()
+  const firstJobTime = new Date(`${year}-${month}-${day}`).getTime();
+
+  const MONTH = (1000 * 60 * 60 * 24 * 30);
+
+  return Math.floor((todayTime - firstJobTime) / MONTH)
+}
+
+const getAmountsForFemale = (totalMonths) => {
+
+  if (typeof value === 'number' && !isNaN(value))
+    throw new Error('the total number of months since you got your first job is required')
+
+  let amountMaximum = null;
+  let amountMinimum = null;
+
+  switch (totalMonths) {
+    case totalMonths <= 26:
+      amountMaximum = AMOUNT_MAXIMUM_MALE['26'][tipoNomina]
+      amountMinimum = AMOUNT_MINIMUM_MALE['26'][tipoNomina]
+      break;
+
+    case totalMonths >= 30:
+      amountMaximum = AMOUNT_MAXIMUM_MALE['30'][tipoNomina]
+      amountMinimum = AMOUNT_MINIMUM_MALE['30'][tipoNomina]
+      break;
+
+    default:
+      amountMaximum = AMOUNT_MAXIMUM_MALE[`${totalMonths}`][tipoNomina]
+      amountMinimum = AMOUNT_MINIMUM_MALE[`${totalMonths}`][tipoNomina]
+      break;
+  }
+
+  return { max: amountMaximum, min: amountMinimum }
+}
+
+const getAmountsForMale = (totalMonths) => {
+
+  if (typeof value === 'number' && !isNaN(value))
+    throw new Error('The total number of months since you got your first job is required')
+
+  let amountMaximum = null;
+  let amountMinimum = null;
+
+  switch (totalMonths) {
+    case totalMonths <= 24:
+      amountMaximum = AMOUNT_MAXIMUM_FEMALE['24'][tipoNomina]
+      amountMinimum = AMOUNT_MINIMUM_FEMALE['24'][tipoNomina]
+      break;
+
+    case totalMonths >= 28:
+      amountMaximum = AMOUNT_MAXIMUM_FEMALE['28'][tipoNomina]
+      amountMinimum = AMOUNT_MINIMUM_FEMALE['28'][tipoNomina]
+      break;
+
+    default:
+      amountMaximum = AMOUNT_MAXIMUM_FEMALE[`${totalMonths}`][tipoNomina]
+      amountMinimum = AMOUNT_MINIMUM_FEMALE[`${totalMonths}`][tipoNomina]
+      break;
+  }
+
+  return { max: amountMaximum, min: amountMinimum }
+}
+
+const getLineaRecommendation = (amountMax, amountMin) => {
+
+  const p1 = amountMin + (Math.sqrt((amountMax - amountMin)))
+  const p2 = amountMin + (0.0175 * (amountMax - amountMin))
+
+  return Math.max(p1, p2)
+}
+
+function calculoMotor(tipoNomina, fechaPrimerEmpleo, genero) {
+
+  const monthsSinceFirstJob = getMonthsSinceFirstJob(fechaPrimerEmpleo)
+
+  if (!(['m', 'f'].includes(genero))) throw new Error('Invalid genre');
+
+  const amount = genero === 'm'
+    ? getAmountsForMale(monthsSinceFirstJob)
+    : getAmountsForFemale(monthsSinceFirstJob)
+
+  const recommendationLinea = getLineaRecommendation(amount.max, amount.min)
+
+  return {
+    'montoMinimo': amount.max,
+    'montoMáximo': amount.min,
+    'recomendacionLinea': recommendationLinea
+  }
+
+}
+
+console.log(calculoMotor('A', '12/06/2022', 'f')) 
