e1c3d85c53386c4d21e34e1e9ce76de9b0b1d0fe
Franklin Martinez Lucas <frankomtzlucas361@gmail.com>
2024-06-18 16:53:59 -0600

    first commit

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ console.log(calculoMotor('B', '30/12/1993', 'f')) // { montoMinimo: 4400, 'monto
 console.log(calculoMotor('C', '19/09/2020', 'm')) // { montoMinimo: 4600, 'montoMáximo': 600, recomendacionLinea: 670 }
 console.log(calculoMotor('D', '15/01/2019', 'm')) // { montoMinimo: 4300, 'montoMáximo': 1000, recomendacionLinea: 1057.75 }
 ```
+
+![results](./image.png)
--- motor.js
+++ motor.js
@@ -216,7 +216,15 @@ function calculoMotor(tipoNomina, fechaPrimerEmpleo, genero) {
 
 }
 
-console.log(calculoMotor('A', '12/06/2022', 'f')) // { montoMinimo: 4000, 'montoMáximo': 800, recomendacionLinea: 856.5685424949238 }
-console.log(calculoMotor('B', '30/12/1993', 'f')) // { montoMinimo: 4400, 'montoMáximo': 700, recomendacionLinea: 764.75 }
-console.log(calculoMotor('C', '19/09/2020', 'm')) // { montoMinimo: 4600, 'montoMáximo': 600, recomendacionLinea: 670 }
-console.log(calculoMotor('D', '15/01/2019', 'm')) // { montoMinimo: 4300, 'montoMáximo': 1000, recomendacionLinea: 1057.75 }
+console.log('\n')
+console.log('A', '12/06/2022', 'f')
+console.table(calculoMotor('A', '12/06/2022', 'f')) // { montoMinimo: 4000, 'montoMáximo': 800, recomendacionLinea: 856.5685424949238 }
+
+console.log('B', '30/12/1993', 'f')
+console.table(calculoMotor('B', '30/12/1993', 'f')) // { montoMinimo: 4400, 'montoMáximo': 700, recomendacionLinea: 764.75 }
+
+console.log('C', '19/09/2020', 'm')
+console.table(calculoMotor('C', '19/09/2020', 'm')) // { montoMinimo: 4600, 'montoMáximo': 600, recomendacionLinea: 670 }
+
+console.log('D', '15/01/2019', 'm')
+console.table(calculoMotor('D', '15/01/2019', 'm')) // { montoMinimo: 4300, 'montoMáximo': 1000, recomendacionLinea: 1057.75 }
